8df0d0c0beff57f23ca5e3e5e3392fb08ab05342
da-handa <wkekgks@gmail.com>
2025-10-09 03:57:14 +0900

    Final fix for Dockerfile executable name

--- target/debug/deps/pink_code_server-5ef02db95a3d5a00.d
+++ target/debug/deps/pink_code_server-5ef02db95a3d5a00.d
@@ -1,5 +1,5 @@
-/home/dahan/pinkcode server/target/debug/deps/pink_code_server-5ef02db95a3d5a00.d: src/main.rs
+/home/dahan/pinkcodeserver/target/debug/deps/pink_code_server-5ef02db95a3d5a00.d: src/main.rs
 
-/home/dahan/pinkcode server/target/debug/deps/libpink_code_server-5ef02db95a3d5a00.rmeta: src/main.rs
+/home/dahan/pinkcodeserver/target/debug/deps/libpink_code_server-5ef02db95a3d5a00.rmeta: src/main.rs
 
 src/main.rs:
